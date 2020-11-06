2d53c3b9a2c4c03a7881c1850d92596511fd833c
Parth Gupta <guptaparth118@gmail.com>
2020-11-06 20:47:42 +0530

    Project Done

--- main.js
+++ main.js
@@ -8,8 +8,24 @@ app.controller('name', function($scope, $rootScope) {
     
 
     $scope.changeHome = function() {
-        $rootScope.heading=$scope.$$watchers[2].last;
-        $rootScope.img = $scope.$$watchers[3].last;
+        $rootScope.heading=$scope.$$watchers[3].last;
+        $rootScope.img = $scope.$$watchers[4].last;
+        $rootScope.content = $scope.$$watchers[0].last
         console.log($scope.$$watchers[1].last);
+    } 
+});
+
+app.controller('movie', function($scope, $rootScope) {
+
+
+    console.log($scope);
+    console.log($rootScope);
+    
+
+    $scope.showMovie = function() {
+        $rootScope.heading=$scope.$$watchers[3].last;
+        $rootScope.img = $scope.$$watchers[4].last;
+        $rootScope.content = $scope.$$watchers[0].last
+        console.log($scope.$$watchers[0].last);
     } 
 });
